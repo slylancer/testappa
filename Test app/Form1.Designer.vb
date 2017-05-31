bebcaa92e224a5e8d1402a98ca8417227a9ef969
Thomas Koerner <sly@jmp.cx>
2017-05-31 14:03:34 -0400

    Add files via upload

---
+++ Test app/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.submitButton = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.SuspendLayout()
+        '
+        'submitButton
+        '
+        Me.submitButton.Location = New System.Drawing.Point(195, 225)
+        Me.submitButton.Name = "submitButton"
+        Me.submitButton.Size = New System.Drawing.Size(83, 30)
+        Me.submitButton.TabIndex = 0
+        Me.submitButton.Text = "Submit"
+        Me.submitButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(268, 199)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.submitButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents submitButton As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Test app/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub submitButton_Click(sender As Object, e As EventArgs) Handles submitButton.Click
+        Close()
+        crap
+    End Sub
+
+End Class
